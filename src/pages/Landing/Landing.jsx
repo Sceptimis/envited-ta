1db33023c49d65d5adcf248f1b5059df38f7687c
Lleyton van Sambeeck <sceptimis@tuta.io>
2022-02-22 12:25:50 +0800

    Add main text to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,3 +1,4 @@
+import styles from './Landing.module.scss'
 import Background from "../../components/Background/Background";
 import NavBar from "../../components/NavBar/NavBar";
 
@@ -6,6 +7,18 @@ function Landing() {
     <>
       <Background />
       <NavBar />
+      <div className={styles.textContent}>
+        <h1 className={styles.mainText}>
+          Facebook events
+          <br />
+          without Facebook.
+        </h1>
+        <h2 className={styles.subText}>
+          Easily host and share events with your
+          <br />
+          friends across any social media.
+        </h2>
+      </div>
     </>
   );
 }
