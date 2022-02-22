b9e2d0144fcb165f386855d84c550333f102daf2
Lleyton van Sambeeck <sceptimis@tuta.io>
2022-02-22 12:15:22 +0800

    Add NavBar component, and add it to landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import Background from "../../components/Background/Background";
+import NavBar from "../../components/NavBar/NavBar";
 
 function Landing() {
   return (
     <>
       <Background />
+      <NavBar />
     </>
   );
 }
